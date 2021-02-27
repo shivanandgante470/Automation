4f9bf14ff701da8748eec97a64e765b51b20c04a
shivanandsagar4 <79081672+shivanandsagar4@users.noreply.github.com>
2021-02-27 17:29:11 +0530

    Update BrowserTest.java

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -8,6 +8,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+//added
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 
 public class BrowserTest {
@@ -17,6 +21,9 @@ public class BrowserTest {
 	public void getData()
 	{
 		System.out.println("Hello Guys");
+		//added
+		driver = webdriver.Chrome(ChromeDriverManager(version="87.0.4280.88").install())
+driver.get("https://www.google.com");
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		WebDriver driver =new ChromeDriver();
 		driver.get("https://rahylshettyacademy.azurewebsites.net/webapp/");
